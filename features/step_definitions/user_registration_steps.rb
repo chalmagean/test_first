18a840677079db767b879fcccbdd4a7fd8596913
Cezar Halmagean <cezar@mixandgo.com>
2018-10-25 14:02:37 +0300

    Add step definitions

---
+++ features/step_definitions/user_registration_steps.rb
@@ -0,0 +1,19 @@
+Given("I am on the homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click on the registration link") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in and submit the registration from") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a registration confirmation message") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should receive a confirmation email") do
+  pending # Write code here that turns the phrase above into concrete actions
+end

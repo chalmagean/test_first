201a31faa502668c365ffe19b9d9429c92a74e65
Cezar Halmagean <cezar@mixandgo.com>
2018-10-29 11:02:07 +0200

    Use underscores instead of hyphens

--- features/step_definitions/user_registration_steps.rb
+++ features/step_definitions/user_registration_steps.rb
@@ -7,11 +7,11 @@ Given("I click on the registration link") do
 end
 
 When("I fill in and submit the registration from") do
-  fill_in "user-name", with: "jdoe"
-  fill_in "user-email", with: "jdoe@example.com"
-  fill_in "user-password", with: "secret1234"
-  fill_in "user-password-confirmation", with: "secret1234"
-  click_on "user-registration-submit"
+  fill_in "user_name", with: "jdoe"
+  fill_in "user_email", with: "jdoe@example.com"
+  fill_in "user_password", with: "secret1234"
+  fill_in "user_password_confirmation", with: "secret1234"
+  click_on "user_registration_submit"
 end
 
 Then("I should see a registration confirmation message") do

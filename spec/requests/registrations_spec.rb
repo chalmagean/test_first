b02e7ce229eda2b52a605c04f24d7b1f710da50f
Cezar Halmagean <cezar@mixandgo.com>
2019-04-25 17:05:10 +0300

    Add registrations request test

---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "Registrations", type: :request do
+  describe "POST /registrations" do
+    it "creates a user in the database" do
+      params = { email: "jdoe@email.com", password: "secret1234" }
+      post user_registration_path, params: params
+      expect(response).to have_http_status(200)
+      expect(User.all.count).to eq(1)
+    end
+  end
+end

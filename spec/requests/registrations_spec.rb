61a5de5ce4090da5c1a094db9049fe1fe5a8648d
Cezar Halmagean <cezar@mixandgo.com>
2019-04-25 17:49:55 +0300

    Wrap the request params

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Registrations", type: :request do
   describe "POST /registrations" do
     it "creates a user in the database" do
       params = { email: "jdoe@email.com", password: "secret1234" }
-      post user_registration_path, params: params
+      post user_registration_path, params: { user: params }
       expect(response).to have_http_status(200)
       expect(User.all.count).to eq(1)
     end

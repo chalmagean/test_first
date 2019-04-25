a536082b5da3a1318dc95491a8c6d933d972cf14
Cezar Halmagean <cezar@mixandgo.com>
2019-04-25 17:53:40 +0300

    Expect 302 instead of 200

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Registrations", type: :request do
     it "creates a user in the database" do
       params = { email: "jdoe@email.com", password: "secret1234" }
       post user_registration_path, params: { user: params }
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
       expect(User.all.count).to eq(1)
     end
   end

57bf41af3e5743b75e19001cc1780d6c6a3e4614
Cezar Halmagean <cezar@mixandgo.com>
2020-01-17 08:04:53 +0200

    Added 100 max limit for name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, length: { maximum: 100 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "when the name is longer than 100 characters" do
+      it "the record is invalid" do
+        user = User.new(name: "a" * 101)
+        user.valid?
+        expect(user.errors.keys).to include(:name)
+      end
+    end
+  end
 end
